456120023c5232f0e9f4d4d1bb36b227817cdc9b
caleb <calebikhuohon@gmail.com>
2018-04-10 10:29:23 +0100

    Initial commit

---
+++ landing.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="app.css">
+</head>
+<body>
+    <main>
+    <div id="rectangle">
+        <h1>Caleb Ikhuohon</h1>
+        <h2>Undergrad and fullstack developer</h2>
+        <div class="time">
+            <p><?php echo date('D F j, g:i:s A'); ?> </p>
+        </div>
+    </div>
+    </main>
+</body>
+</html>
